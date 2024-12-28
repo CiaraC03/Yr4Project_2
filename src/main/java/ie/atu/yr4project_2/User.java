201ae01d8a16b95f18b3783f43c4e64d54245eb0
ciara <g00402357@atu.ie>
2024-12-28 12:28:46 +0000

    Added user class with task variables. Used lombok

--- src/main/java/ie/atu/yr4project_2/User.java
+++ src/main/java/ie/atu/yr4project_2/User.java
@@ -1,2 +1,36 @@
-package ie.atu.yr4project_2;public class User {
+package ie.atu.yr4project_2;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Title can't be blank")
+    private String title;
+    @NotBlank(message = "Description can't be blank")
+    private String description;
+    @FutureOrPresent(message = "Date cannot be in the past")
+    private LocalDateTime dueDate;
+    @NotBlank(message = "Priority can't be blank")
+    private int priority;
+
+
+
+
+
+
 }
