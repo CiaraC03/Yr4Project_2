b1314e553225c170b536a3d59ca91781b5cacf00
ciara <g00402357@atu.ie>
2025-01-06 22:09:49 +0000

    Updates

--- src/main/java/ie/atu/yr4project_2/Task.java
+++ src/main/java/ie/atu/yr4project_2/Task.java
@@ -1,29 +1,32 @@
 package ie.atu.yr4project_2;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "tasks")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "User Id can't be blank")
+    private Long userId;
     @NotBlank(message = "Title can't be blank")
     private String title;
     @NotBlank(message = "Description can't be blank")
     private String description;
     @FutureOrPresent(message = "Date cannot be in the past")
     private LocalDateTime dueDate;
-    @NotBlank(message = "Priority can't be blank")
+    @NotNull(message = "Priority can't be blank")
     private int priority;
 }
--- src/main/java/ie/atu/yr4project_2/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ie.atu.yr4project_2;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository <Task, Long>
-{
-}
